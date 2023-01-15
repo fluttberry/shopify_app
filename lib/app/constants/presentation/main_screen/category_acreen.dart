1a167ae11bb35ff297e029a737c3dbaba74de364
fluttberry <srvffx@gmail.com>
2023-01-15 20:26:52 +0600

    updated

--- lib/app/constants/presentation/main_screen/category_acreen.dart
+++ lib/app/constants/presentation/main_screen/category_acreen.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:shopify_app/app/constants/colors/app_colors.dart';
+import 'package:shopify_app/app/constants/presentation/main_screen/fake_search_widget.dart';
 
-List<String> listIndex = ['men', 'women'];
+List<ItemsData> items = [
+  ItemsData(lable: 'men'),
+  ItemsData(lable: 'women'),
+  ItemsData(lable: 'shoes'),
+  ItemsData(lable: 'bads'),
+  ItemsData(lable: 'electronicts'),
+  ItemsData(lable: 'accessories'),
+  ItemsData(lable: 'home & garden'),
+  ItemsData(lable: 'kids'),
+  ItemsData(lable: 'beauty'),
+];
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -11,13 +22,19 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  final _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: AppColors.white,
+        title: const FakeSearchWidget(),
+      ),
       body: Stack(children: [
         Positioned(bottom: 0, left: 0, child: sideNavigator(size)),
-        Positioned(bottom: 0, left: 0, child: categoryView(size)),
+        Positioned(bottom: 0, right: 0, child: categoryView(size)),
       ]),
     );
   }
@@ -27,13 +44,27 @@ class _CategoryScreenState extends State<CategoryScreen> {
       height: size.height * 0.8,
       width: size.width * 0.2,
       child: ListView.builder(
-        itemCount: 4,
+        itemCount: items.length,
         itemBuilder: ((context, index) {
           return GestureDetector(
-            onTap: () {},
+            onTap: () {
+              for (var element in items) {
+                
+              }
+              setState(() {
+                items[index].isSelected = true;
+              });
+              _pageController.jumpToPage(index);
+            },
             // ignore: avoid_unnecessary_containers
             child: Container(
-              child: const Text('men'),
+              height: 100,
+              color: items[index].isSelected == true
+                  ? AppColors.white
+                  : AppColors.yellow,
+              child: Center(
+                child: Text(items[index].lable),
+              ),
             ),
           );
         }),
@@ -43,22 +74,40 @@ class _CategoryScreenState extends State<CategoryScreen> {
 
   Widget categoryView(Size size) {
     return Container(
-      color: AppColors.grey,
+      color: AppColors.white,
       height: size.height * 0.8,
-      width: size.width * 0.2,
-      child: PageView(scrollDirection: Axis.vertical, children: const [
-        Center(child: Text('men category')),
-        Center(child: Text('women category')),
-        Center(child: Text('shoes category')),
-        Center(child: Text('bags category')),
-        Center(child: Text('electronics category')),
-        Center(child: Text('accessories category')),
-        Center(child: Text('home & garden category')),
-        Center(child: Text('men category')),
-        Center(child: Text('kids category')),
-        Center(child: Text('beauty category')),
-      ]),
+      width: size.width * 0.8,
+      child: PageView(
+          controller: _pageController,
+          scrollDirection: Axis.vertical,
+          children: const [
+            Center(child: Text('men category')),
+            Center(child: Text('women category')),
+            Center(child: Text('shoes category')),
+            Center(child: Text('bags category')),
+            Center(child: Text('electronics category')),
+            Center(child: Text('accessories category')),
+            Center(child: Text('home & garden category')),
+            Center(child: Text('men category')),
+            Center(child: Text('kids category')),
+            Center(child: Text('beauty category')),
+          ]),
     );
   }
 }
+
+class ItemsData {
+  String lable;
+  bool isSelected;
+  ItemsData({
+    required this.lable,
+    this.isSelected = false,
+  });
+}
+
+
+
+
+
+
 //https://www.youtube.com/watch?v=b1DLFKf7gCo 1.09.00
