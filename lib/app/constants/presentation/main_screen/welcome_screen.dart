0ac6ed6c26e940724b7b1990cb69e08bcf96af3e
fluttberry <srvffx@gmail.com>
2023-01-26 18:09:46 +0600

    welcome screen animations

--- lib/app/constants/presentation/main_screen/welcome_screen.dart
+++ lib/app/constants/presentation/main_screen/welcome_screen.dart
@@ -1,6 +1,17 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:shopify_app/app/constants/colors/app_colors.dart';
+
 import 'package:shopify_app/app/constants/decoration/app_decoration.dart';
+
+const textColors = [
+  Colors.yellowAccent,
+  Colors.red,
+  Colors.blueAccent,
+  Colors.yellowAccent,
+  Colors.green,
+  Colors.green,
+  Colors.teal,
+];
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -19,12 +30,41 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         // ignore: prefer_const_literals_to_create_immutables
         child: SafeArea(
           child: Column(
-            children: const [
-              Text(
-                'WELCOME',
-                style: TextStyle(
-                  color: AppColors.white,
-                  fontSize: 30,
+            children: [
+              // ignore: prefer_const_constructors
+              // Text(
+              //   'WELCOME',
+              //   style: const TextStyle(
+              //     color: AppColors.white,
+              //     fontSize: 30,
+              //   ),
+              // ),
+              AnimatedTextKit(
+                repeatForever: true,
+                animatedTexts: [
+                  ColorizeAnimatedText(
+                    'Welcome',
+                    textStyle: const TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Acme'),
+                    colors: textColors,
+                  ),
+                  ColorizeAnimatedText(
+                    'Duck Store',
+                    textStyle: const TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Acme'),
+                    colors: textColors,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 120,
+                width: 200,
+                child: Image(
+                  image: AssetImage('images/inapp/logo.jpg'),
                 ),
               ),
             ],
@@ -34,3 +74,4 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     );
   }
 }
+//https://www.youtube.com/watch?v=sAX1b-b3dLE 38
