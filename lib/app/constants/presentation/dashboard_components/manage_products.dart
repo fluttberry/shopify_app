8b89bd64bade1c835701a4a0d567c1199b05cfc6
fluttberry <srvffx@gmail.com>
2023-01-29 15:32:43 +0600

    dashboard page is completed

---
+++ lib/app/constants/presentation/dashboard_components/manage_products.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shopify_app/app/constants/colors/app_colors.dart';
+import 'package:shopify_app/app/constants/presentation/widgets/app_bar_widgets/appbar_back_button_widget.dart';
+import 'package:shopify_app/app/constants/presentation/widgets/app_bar_widgets/appbar_title_widget.dart';
+
+class ManageProducts extends StatelessWidget {
+  const ManageProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppColors.white,
+        title: const AppbarTitleWidget(
+          title: 'Manage Products',
+        ),
+        leading: const AppbarBackButtonWidget(),
+      ),
+    );
+  }
+}
--- lib/app/constants/presentation/main_screen/dashboar_screen.dart
+++ lib/app/constants/presentation/main_screen/dashboar_screen.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:shopify_app/app/constants/colors/app_colors.dart';
+import 'package:shopify_app/app/constants/presentation/dashboard_components/balance.dart';
+import 'package:shopify_app/app/constants/presentation/dashboard_components/edit_profile.dart';
+import 'package:shopify_app/app/constants/presentation/dashboard_components/manage_products.dart';
+import 'package:shopify_app/app/constants/presentation/dashboard_components/my_store.dart';
+import 'package:shopify_app/app/constants/presentation/dashboard_components/orders.dart';
+import 'package:shopify_app/app/constants/presentation/dashboard_components/statics.dart';
 import 'package:shopify_app/app/constants/presentation/widgets/app_bar_widgets/appbar_title_widget.dart';
+
+List<IconData> icons = [
+  Icons.store,
+  Icons.shop_2_outlined,
+  Icons.edit,
+  Icons.settings,
+  Icons.attach_money,
+  Icons.show_chart,
+];
+List<String> lable = [
+  'my store'
+      'order'
+      'edit profile'
+      'manage products'
+      'balance'
+      'statics'
+];
+List<Widget> pages = [
+  const MyStore(),
+  const Orders(),
+  const EditProfile(),
+  const ManageProducts(),
+  const Balance(),
+  const Statics(),
+];
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
@@ -26,34 +57,52 @@ class DashboardScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(
-          6,
-          (index) {
-            return Card(
-              child: Column(
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  const Icon(
-                    Icons.store,
-                    size: 50,
-                    color: AppColors.yellowAccent,
-                  ),
-                  const Text(
-                    'MyStore',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: AppColors.yellowAccent,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 2,
-                      fontFamily: 'Acme',
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: GridView.count(
+          mainAxisSpacing: 50,
+          crossAxisSpacing: 50,
+          crossAxisCount: 2,
+          children: List.generate(
+            6,
+            (index) {
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => pages[index],
                     ),
+                  );
+                },
+                child: Card(
+                  shadowColor: AppColors.purpleAccent,
+                  elevation: 20,
+                  color: AppColors.blueGrey.withOpacity(0.7),
+                  child: Column(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Icon(
+                        icons[index],
+                        size: 50,
+                        color: AppColors.yellowAccent,
+                      ),
+                      Text(
+                        lable[index].toUpperCase(),
+                        style: const TextStyle(
+                          fontSize: 24,
+                          color: AppColors.yellowAccent,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 2,
+                          fontFamily: 'Acme',
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            );
-          },
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
